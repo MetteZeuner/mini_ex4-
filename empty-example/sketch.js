9cc36f1e68235dd66326ba7443374d21000756ce
MetteZeuner <mette.zeuner@outlook.dk>
2017-03-05 19:09:26 +0100

    Update sketch.js

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,5 +1,5 @@
 var circle = {
-    x:400, 
+    x:400, // 
     y: 200,
     xgo: 4,
     ygo: -3
@@ -25,22 +25,22 @@ ellipse(circle.x, circle.y, 30, 30);
 
     
 if(circle.x > width || circle.x < 0){
-    circle.xgo = circle.xgo * -1;
+    circle.xgo = circle.xgo * -1; 
 }
 
 if(circle.y > height || circle.y < 0){
-    circle.ygo = circle.ygo * -1;
+    circle.ygo = circle.ygo * -1; //these 'if conditions' is what makes the circle bounce from wall to wall. 
 }
    
-    circle.x = circle.x + circle.xgo;
-    circle.y = circle.y + circle.ygo;    
+    circle.x = circle.x + circle.xgo; // this make sure that the circle goes forward unless it hits on of the walls.   
+    circle.y = circle.y + circle.ygo; // this make sure that the circle goes forward unless it hits on of the walls. 
        
 }
 
 function mousePressed(){
-    r = random(255);
+    r = random(255); //this makes sure that the circle changes colour when the mouse is pressed. 
     g = random(255);
     b = random(255);
     
-    circle.xgo = circle.xgo * -1;
+    circle.xgo = circle.xgo * -1; //this makes the circle change direction when the mouse is pressed. 
 }
